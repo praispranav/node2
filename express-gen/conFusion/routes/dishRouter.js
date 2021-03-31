7f8c0ae7416f0f2c3269d25b1d2718551ee1c19d
Pranav Kumar <praispranav@gmail.com>
2021-03-31 15:41:43 +0530

    mongoose_ODM_2

--- express-gen/conFusion/routes/dishRouter.js
+++ express-gen/conFusion/routes/dishRouter.js
@@ -86,4 +86,128 @@ dishRouter.route('/:dishId')
     .catch((err) => next(err) )
 });
 
+
+dishRouter.route('/:dishId/comments')
+.get((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        if(dish != null){
+            res.statusCode= 200;
+            res.setHeader('Content-Type',"application/json");
+            res.json(dish.comments)
+        }
+        else {
+            err = new Error("Comments Not Found");
+             err.statusCode = 404;
+            return next(err)
+        }
+    },(err)=> next(err))
+    .catch((err)=> next(err))
+})
+.post((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        if (dish !=null){
+            res.statusCode= 200;
+            res.setHeader('Content-Type',"application/json");
+            dish.comments.push(req.body)
+            dish.save()
+                .then((com)=>{
+                    res.statusCode= 200;
+                    res.setHeader('Content-Type',"application/json");
+                    res.json(com)
+                },(err)=> next(err))
+        }
+        else{
+            err = new Error("Comments can be added to an error occured");
+            err.statusCode = 404;
+            return next(err)
+        }
+    },(err)=> next(err))
+    .catch((err)=> next(err))
+})
+.delete((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        if(dish !=null){
+            res.statusCode= 200;
+            res.setHeader('Content-Type',"application/json");
+            dish.comments.deleteMany();
+            res.end("All comments were deleted related to :"+ req.params.dishId)
+        }
+        else{
+            err = new Error("Comments Cannot be deleted");
+            err.statusCode = 404;
+            return next(err)
+        }
+    },(err) => next(err))
+    .catch((err)=> next(err))
+})
+.put((req,res,next)=>{
+    res.end("This operation cannot be done")
+});
+
+dishRouter.route('./:dishId/comments/:commentId')
+.get((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        if(dish != null && dish.comments._id(req.params.commentId)){
+            res.statusCode= 200;
+            res.setHeader('Content-Type',"application/json");
+            res.json(dish.comments.findById(req.params.commentId))
+        }
+        else {
+            err = new Error("Comments Not Found");
+             err.statusCode = 404;
+            return next(err)
+        }
+    },(err)=> next(err))
+    .catch((err)=> next(err))
+})
+.post((req,res,next)=>{
+    res.end("This operation is not supported ")
+})
+.delete((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        if(dish !=null && dish.comments.id(req.params.commentId)){
+            res.statusCode= 200;
+            res.setHeader('Content-Type',"application/json");
+            dish.comments.id(req.params.commentId).deleteMany();
+            res.end("All comments were deleted related to :"+ req.params.commentId)
+        }
+        else{
+            err = new Error("Comments Cannot be deleted");
+            err.statusCode = 404;
+            return next(err)
+        }
+    },(err) => next(err))
+    .catch((err)=> next(err))
+})
+.put((req,res,next)=>{
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        if (dish !=null && dish.comments.id(req.params.commentId)){
+            if (req.body.rating){
+                dish.comments.id(req.params.commentId).rating = req.body.rating
+            }
+            if(req.body.comment){
+                dish.comments.id(req.params.commentId).comment = req.body.comment
+            }
+            dish.save()
+                .then((com)=>{
+                    res.statusCode= 200;
+                    res.setHeader('Content-Type',"application/json");
+                    res.json(com)
+                },(err)=> next(err))
+        }
+        else{
+            err = new Error("Comments can be added to an error occured");
+            err.statusCode = 404;
+            return next(err)
+        }
+    },(err)=> next(err))
+    .catch((err)=> next(err))
+});
+
 module.exports = dishRouter;
